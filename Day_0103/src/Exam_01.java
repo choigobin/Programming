90876bcf79163e074025a9766c04408293109391
CHOI KANGMIN <54352094+choigobin@users.noreply.github.com>
2020-01-03 19:05:23 +0900

    Update_0103
    for문 while문 do while문 연습

--- Day_0103/src/Exam_01.java
+++ Day_0103/src/Exam_01.java
@@ -6,16 +6,26 @@
  */
 public class Exam_01 {
 	public static void main(String[] args) {
-		// 1~10 출력
-		int i = 0;
 		
+		// 1 ~ 10 출력
+		int i = 0;	
 		while(true) {
 			i++;
 			System.out.println(i);
-			if(i==10) break;
+			if(i==10) {
+				break;
+			}
+		}
+			
+		while(i<10) {
+			i++;
+			System.out.println(i);
 		}
 		
-		
-		
+		for(int x = 1; x <= 10; x++) {
+			System.out.println(x);
+		}
+			
 	}
+
 }
