2010d3670ed80478fad2e1d3a95ddb79f3fa675b
cc <cc>
2020-05-29 16:00:07 +0900

    CreateErrorPage

--- ExSpring/Exam_03/src/main/java/com/jslhrd/exception/CommonExceptionAdvice.java
+++ ExSpring/Exam_03/src/main/java/com/jslhrd/exception/CommonExceptionAdvice.java
@@ -1,8 +1,11 @@
 package com.jslhrd.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,6 +20,13 @@ public class CommonExceptionAdvice {
 		model.addAttribute("exception",ex);
 		log.error(model.toString());
 		return "error_page";
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handle404(NoHandlerFoundException ex) {
+		
+		return "custom404";
 	}
 	
 }
